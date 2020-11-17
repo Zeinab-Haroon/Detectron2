912f47c84af4afd1cc09a5141bd11d1c1d9903dc
Zeinab Haroon <58106230+Zeinab-Haroon@users.noreply.github.com>
2020-11-17 10:44:26 +0200

    Update cityscapes.py

--- detectron2/data/datasets/cityscapes.py
+++ detectron2/data/datasets/cityscapes.py
@@ -31,10 +31,10 @@ def _get_cityscapes_files(image_dir, gt_dir):
     logger.info(f"{len(cities)} cities found in '{image_dir}'.")
     print('len cities', len(cities))
     for city in cities:
-    #     print('city', city)
-    #     wanted = 'berlin'
-    #     if city != wanted:
-    #         continue
+        print('city', city)
+        wanted = 'berlin'
+        if city != wanted:
+            continue
         city_img_dir = os.path.join(image_dir, city)
         city_gt_dir = os.path.join(gt_dir, city)
         for basename in PathManager.ls(city_img_dir):
