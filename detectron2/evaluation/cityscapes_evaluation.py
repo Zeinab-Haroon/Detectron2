72d2b2911dd64ae8e1f7d08ae034c3b6b90b4ea4
Zeinab Haroon <58106230+Zeinab-Haroon@users.noreply.github.com>
2020-11-20 13:07:57 +0200

    Update cityscapes_evaluation.py

--- detectron2/evaluation/cityscapes_evaluation.py
+++ detectron2/evaluation/cityscapes_evaluation.py
@@ -115,7 +115,7 @@ class CityscapesInstanceEvaluator(CityscapesEvaluator):
             cityscapes_eval.args.groundTruthSearch
         )
         predictionImgList = []
-        wanted = 'berlin'
+        wanted = 'frankfurt'
         for gt in groundTruthImgList:
             if gt.split('/')[4] != wanted:
                 continue
